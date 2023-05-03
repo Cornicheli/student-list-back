8c42520ef4d44277b4f2ea6296932fb2f23d4050
Agustin Cornide <gabrielcornide.pg@gmail.com>
2023-05-03 17:06:17 -0300

    config, data, models creados

---
+++ data/alumnos.js
@@ -0,0 +1,29 @@
+let alumnos = [
+    {
+        "nombre" : "gabriel",
+        "apellido": "cornide",
+        "edad": 6
+    },
+    {
+        "nombre" : "agustin",
+        "apellido": "cornide",
+        "edad": 8
+    },
+    {
+        "nombre" : "thiago",
+        "apellido": "cornide",
+        "edad": 8
+    },
+]
+
+require('dotenv').config()
+require('../config/database')
+const Alumnos = require("../models/Alumnos")
+
+alumnos.forEach((e) => {
+    Alumnos.create({
+        nombre: e.nombre,
+        apellido: e.apellido,
+        edad: e.edad,
+    });
+  });
---
+++ models/Profesor.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    nombre: {type: String, required: true},
+    apellido: {type: String, required: true},
+    role: {type: String, required: true},
+    id: {type: mongoose.Types.ObjectId, required: true}
+
+})
+
+const Profesor = mongoose.model('profesor', schema)
+module.exports = Profesor
